a48133009c7cf34ed491d27dad6e74ef4f2d9d36
github username <github email address>
2017-10-24 09:53:46 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,27 @@
 # Enter your procedural solution here!
+
+  # while i < 1000
+  #   add_it_up = []
+  #   i = 0
+  #
+  #   if i%3 == 0 || i%5 == 0
+  #     add_it_up.push(i)
+  #     i += 1
+  #   end
+  #   a = 0
+  #
+  #   add_it_up.map |x|
+  #     a += x
+  # end
+
+  def collect_multiples(limit)
+    range = (3...limit).to_a
+    range.select do |x|
+      x%3==0 || x%5==0
+    end
+  end
+
+  def sum_multiples(limit)
+    array = collect_multiples(limit)
+    array.inject(0, :+)
+  end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,20 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+      range = (3...self.limit).to_a
+      range.select do |x|
+        x%3==0 || x%5==0
+      end
+    end
+
+    def sum_multiples
+      array = collect_multiples
+      array.inject(0, :+)
+    end
+  end
